463788a54fd5f1323f008c69a05afbf3d56c15a8
Nguyen Ba Quan <quanneprvt@gmail.com>
2019-11-09 11:30:03 +0700

    * Update scr

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private GameObject m_Target;
+    private Vector3 offset;
+    private Vector3 pos;
+    float minX = 0f;
+    float maxX = 4057f;
+    float maxY = 0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        // offset = transform.position - Player.transform.position;
+        pos =  transform.position;
+        offset = transform.position - m_Target.transform.position;
+        //
+        minX = 0.75f;
+        maxX = 18f;
+        maxY = transform.position.y - 0.5f;
+    }
+
+    //Update each frame
+    void Update()
+    {
+        // offset.x = Player.transform.position.x - transform.position.x;
+        //
+        // pos = transform.position + 3* offset * Time.deltaTime;
+        // pos.x = Math.Min(maxX, Math.Max(minX, transform.position.x + 3 * (m_Target.transform.position.x - transform.position.x) * Time.deltaTime));
+        // pos.y = Math.Min(maxY , transform.position.y + 5 * (m_Target.transform.position.y - (transform.position.y - offset.y - 0.5f)) * Time.deltaTime);
+        pos.x = transform.position.x + 3 * (m_Target.transform.position.x - transform.position.x) * Time.deltaTime;
+        pos.y = transform.position.y + 5 * (m_Target.transform.position.y - transform.position.y) * Time.deltaTime;
+        //
+        transform.position = pos;
+    }
+
+    // LateUpdate is called after Update each frame
+    // void LateUpdate()
+    // {
+    //     transform.position = transform.position + 3* offset * Time.fixedDeltaTime;
+    // }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController controller;
+    public float moveSpeed = 50f;
+    // public Animator animator;
+    //
+    float hMove = 0f;
+    bool isJump = false;
+    bool isCrouch = false;
+    bool isGrounded = false;
+    private float width;
+    private float height;
+    Rigidbody2D r;
+    Vector2 v;
+    //
+    private Vector3 savedStat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        savedStat = transform.position;
+        r = controller.GetRigid();
+        isGrounded = controller.IsLanded();
+        //
+        width = (float)Screen.width / 2.0f;
+        height = (float)Screen.height / 2.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // hMove = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        // r = controller.GetRigid();
+        // isGrounded = controller.IsLanded();
+        // //
+        // // animator.SetBool("Grounded", isGrounded);
+        // // animator.SetFloat("Move Speed", Math.Abs(hMove));
+        // // animator.SetFloat("Verticle Velocity", r.velocity.y);
+        // //
+        // if (Input.GetButtonDown("Jump"))
+        //     isJump = true;
+        // //
+        // if (Input.GetButtonDown("Crouch"))
+        //     isCrouch = true;
+        // else if (Input.GetButtonUp("Crouch"))
+        //     isCrouch = false;
+        //
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);    
+            Vector3 point = ray.origin + (ray.direction * Camera.main.transform.position.z * -1);
+            point.z = 0;
+            Debug.Log(point);
+            transform.position = point;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(hMove * Time.fixedDeltaTime, isCrouch, isJump);
+        //
+        if (isJump)
+            isJump = false;
+    }
+
+    void Reset()
+    {
+        transform.position = savedStat;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        // Debug.Log(col.gameObject.name);
+        if (col.gameObject.tag == "dead")
+        {
+            this.Reset();
+        }
+    }
+}
