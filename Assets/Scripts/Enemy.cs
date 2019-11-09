8fc356e6765f3d5570f1494793fe571a9f9391cc
xuwoan <xuwoan@gmail.com>
2019-11-09 21:06:42 +0700

    add item enemy platform

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    public float distance;
+    private bool movingRight = true;
+
+    public Transform groundDetection;
+    
+    public int State;
+    private Rigidbody2D myRig;
+    private float timer;
+    [SerializeField]
+    private List<GameObject> item_prefab;
+    [SerializeField]
+    private Transform parentItem;
+  
+
+    public void SetState(int state)
+    {
+        State = state;
+        switch (state)
+        {
+            case GameConstants.ENEMY_STATE_MOVING:
+                timer = GameConstants.TIMER_ENEMY_MOVING;
+                break;
+            case GameConstants.ENEMY_STATE_THROW_BULLET:
+                StartCoroutine(SpawnBullet());
+                timer = GameConstants.TIMER_ENEMY_BULLET;
+                break;
+            case GameConstants.ENEMY_STATE_MOVING_2:
+                timer = GameConstants.TIMER_ENEMY_MOVING_2;
+                break;
+            case GameConstants.ENEMY_STATE_HIDDEN:
+                timer = GameConstants.TIMER_ENEMY_HIDDEN;
+                break;
+            case GameConstants.ENEMY_STATE_SHOWING:
+                timer = GameConstants.TIMER_ENEMY_SHOWING;
+                break;
+
+
+        }
+    }
+    void Start()
+    {
+        SetState(GameConstants.ENEMY_STATE_MOVING);
+    }
+
+    public IEnumerator SpawnBullet()
+    {
+        for(int i=0;i<GameConstants.ITEM_SPAWN_NUMER;i++)
+        {
+            GameObject item= Instantiate(item_prefab[Random.Range(0, item_prefab.Count)]);
+            item.transform.position =  transform.position + new Vector3(0,0.5f,0);
+            item.transform.SetParent(parentItem);
+            float angle = 0;
+            if (movingRight)
+            {
+                angle = Random.Range(30f, 60f);
+            }
+            else
+            {
+                angle = -Random.Range(30f, 60f);
+            }
+            item.GetComponent<Item>().Throw(angle);
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+    void Moving()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance);
+        if (groundInfo.collider == false)
+        {
+            if (movingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        switch (State)
+        {
+            case GameConstants.ENEMY_STATE_MOVING:
+                if(timer>0)
+                {
+                    timer -= Time.deltaTime;
+                    Moving();
+                    if(timer<0)
+                    SetState(GameConstants.ENEMY_STATE_THROW_BULLET);
+                }
+                
+                break;
+            case GameConstants.ENEMY_STATE_THROW_BULLET:
+
+                if (timer > 0)
+                {
+                    timer -= Time.deltaTime;
+                    if (timer < 0)
+                        SetState(GameConstants.ENEMY_STATE_MOVING_2);
+                }
+                break;
+            case GameConstants.ENEMY_STATE_MOVING_2:
+                if (timer > 0)
+                {
+                    timer -= Time.deltaTime;
+                    Moving();
+                    if (timer < 0)
+                        SetState(GameConstants.ENEMY_STATE_HIDDEN);
+                }
+                break;
+            case GameConstants.ENEMY_STATE_HIDDEN:
+                if (timer > 0)
+                {
+                    timer -= Time.deltaTime;
+                   
+                    SetState(GameConstants.ENEMY_STATE_SHOWING);
+                }
+                break;
+            case GameConstants.ENEMY_STATE_SHOWING:
+                if (timer > 0)
+                {
+                    timer -= Time.deltaTime;
+                    Moving();
+                    if (timer < 0)
+                        SetState(GameConstants.ENEMY_STATE_MOVING);
+                }
+                break;
+
+
+        }
+       
+    }
+}
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int State;
+    private Rigidbody2D myRig;
+    private float jumpForce = 10;
+    public float Type ;
+    public void SetState(int state)
+    {
+        State = state;
+        switch (state)
+        {
+         
+            case GameConstants.ITEM_STATE_FALLING:
+
+                Type = GameConstants.ITEM_TYPE_BULLET;
+                break;
+            case GameConstants.ITEM_STATE_GROUND:
+                Type = GameConstants.ITEM_TYPE_GOOD;
+                break;
+            case GameConstants.ITEM_STATE_TAKING:
+              
+                break;
+           
+
+        }
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (Type != GameConstants.ITEM_TYPE_GOOD)
+        {
+            if (other.gameObject.tag == "LeftWall")
+            {
+
+                myRig.velocity = Vector2.right * Random.Range(1f, 2.5f);
+            }
+            else
+             if (other.gameObject.tag == "RightWall")
+            {
+
+                myRig.velocity = Vector2.left * Random.Range(2.5f, 3.5f);
+            }
+            else
+             if (other.gameObject.tag == "FlatForm")
+            {
+
+                myRig.velocity = Vector3.zero;
+                SetState(GameConstants.ITEM_STATE_GROUND);
+            }
+            else
+             if (other.gameObject.tag == "SideWall")
+            {
+                myRig.velocity = Vector3.zero;
+                SetState(GameConstants.ITEM_STATE_GROUND);
+            }
+            else
+             if (other.gameObject.tag == "SideWall")
+            {
+                myRig.velocity = Vector3.zero;
+                SetState(GameConstants.ITEM_STATE_GROUND);
+            }
+            else
+             if (other.gameObject.tag == "Bullet")
+            {
+                if (other.gameObject.GetComponent<Item>().Type == GameConstants.ITEM_TYPE_GOOD)
+                {
+                    myRig.velocity = Vector3.zero;
+                    SetState(GameConstants.ITEM_STATE_GROUND);
+                }
+              //  other.gameObject.GetComponent<Item>().Type = GameConstants.ITEM_TYPE_GOOD;
+            }
+        }
+    }
+    void Awake()
+    {
+
+        myRig = GetComponent<Rigidbody2D>();
+    }
+    void Start()
+    {
+      //  myRig.velocity = (Vector2)(Quaternion.Euler(0, 0, 50f) * Vector2.right) * jumpForce;
+        // myRig.AddForce(new Vector2(0.2f, 0.2f));
+        //  Debug.Log(Quaternion.Euler(0, 0, 60) * Vector2.right);
+
+    }
+    public void Throw(float angle)
+    {
+       
+        Vector2 vector = new Vector2();
+        if (angle < 0)
+            vector = Vector2.left;
+        else vector = Vector2.right;
+        myRig.velocity = (Vector2)(Quaternion.Euler(0, 0, angle) * vector) * jumpForce;
+        SetState(GameConstants.ITEM_STATE_FALLING);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
